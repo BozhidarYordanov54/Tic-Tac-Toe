9a5e7a797209e510418272f1fdff74aa908fe9f7
BozhidarYordanov54 <smvh@abv.bg>
2023-06-22 22:01:03 +0300

    Update Source.cpp

--- Tic-Tac-Toe/Source.cpp
+++ Tic-Tac-Toe/Source.cpp
@@ -56,14 +56,11 @@ int main()
     Shapes::DrawShape shape;
     sf::Event event;
 
-    unsigned int turns = 0;
-    std::vector<Shapes::DrawShape> circlePositions;
-    std::vector<unsigned int> exesPositions;
-    std::vector<unsigned int> coveredPositions;
+    std::vector<sf::Vector2i> circlePositions;
+    std::vector<sf::Vector2i> exesPositions;
 
     while (window.isOpen())
     {
-
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
@@ -73,14 +70,66 @@ int main()
         }
 
         window.clear(sf::Color::White);
+
         shape.drawGrid(gridSize, window);
 
-        //Update the frame
+        if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+        {
+            unsigned int pos = mousePos(event);
+            int row = pos % 10 - 1;
+            int column = pos / 10 - 1;
 
-        shape.drawCircle(mousePos(event) % 10 - 1, mousePos(event) / 10 - 1, gridSize, window, windowWidth, windowHeight);
-        shape.drawCross(mousePos(event) % 10 - 1, mousePos(event) / 10 - 1, gridSize, window, windowWidth, windowHeight);
-       
-        //End update
+            if (row >= 0 && column >= 0)
+            {
+                bool positionOccupied = false;
+
+                for (const auto& position : circlePositions)
+                {
+                    if (position.x == row && position.y == column)
+                    {
+                        positionOccupied = true;
+                        break;
+                    }
+                }
+
+                if (!positionOccupied)
+                {
+                    for (const auto& position : exesPositions)
+                    {
+                        if (position.x == row && position.y == column)
+                        {
+                            positionOccupied = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!positionOccupied)
+                {
+                    if (circlePositions.size() == exesPositions.size())
+                    {
+                        circlePositions.push_back(sf::Vector2i(row, column));
+                    }
+                    else
+                    {
+                        exesPositions.push_back(sf::Vector2i(row, column));
+                    }
+                }
+            }
+        }
+
+        // Draw circles
+        for (const auto& position : circlePositions)
+        {
+            shape.drawCircle(position.x, position.y, gridSize, window, windowWidth, windowHeight);
+        }
+
+        // Draw exes
+        for (const auto& position : exesPositions)
+        {
+            shape.drawCross(position.x, position.y, gridSize, window, windowWidth, windowHeight);
+        }
+
         window.display();
     }
 
