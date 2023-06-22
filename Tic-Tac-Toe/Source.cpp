9c46f2ae93f2ab8d1e719b086893779a3ea93746
BozhidarYordanov54 <smvh@abv.bg>
2023-06-22 19:16:36 +0300

    Added mouse position

--- Tic-Tac-Toe/Source.cpp
+++ Tic-Tac-Toe/Source.cpp
@@ -6,13 +6,61 @@ const unsigned int gridSize = 150;
 const unsigned int windowWidth = 450;
 const unsigned int windowHeight = 450;
 
+unsigned int mousePos(sf::Event& event)
+{
+    unsigned int value = 0;
+
+    if (event.type == sf::Event::MouseButtonPressed)
+    {
+        if (event.mouseButton.button == sf::Mouse::Left)
+        {
+            unsigned int posX = event.mouseButton.x;
+            unsigned int posY = event.mouseButton.y;
+
+            if (posX < gridSize)
+            {
+                value += 10;
+            }
+            else if (posX < gridSize * 2)
+            {
+                value += 20;
+            }
+            else
+            {
+                value += 30;
+            }
+
+            if (posY < gridSize)
+            {
+                value += 1;
+            }
+            else if (posY < gridSize * 2)
+            {
+                value += 2;
+            }
+            else
+            {
+                value += 3;
+            }
+        }
+    }
+
+    std::cout << value << std::endl;
+    return value;
+}
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Tic-Tac-Toe");
 
     Shapes::DrawShape shape;
-
     sf::Event event;
+
+    unsigned int turns = 0;
+    std::vector<Shapes::DrawShape> circlePositions;
+    std::vector<unsigned int> exesPositions;
+    std::vector<unsigned int> coveredPositions;
+
     while (window.isOpen())
     {
 
@@ -29,8 +77,8 @@ int main()
 
         //Update the frame
 
-        shape.drawCircle(0, 0, gridSize, window, windowWidth, windowHeight);
-        shape.drawCross(1, 1, gridSize, window, windowWidth, windowHeight);
+        shape.drawCircle(mousePos(event) % 10 - 1, mousePos(event) / 10 - 1, gridSize, window, windowWidth, windowHeight);
+        shape.drawCross(mousePos(event) % 10 - 1, mousePos(event) / 10 - 1, gridSize, window, windowWidth, windowHeight);
        
         //End update
         window.display();
